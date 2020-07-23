6b13bce2e53c01baeae91d91687bf733b6c40c3c
Andrea Minhas <andrea.minhas@gmail.com>
2020-07-23 15:22:01 -0500

    undo last commit

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,7 +8,7 @@ require 'combustion'
 
 Combustion.initialize! :active_record,
                        database_reset: false,
-                       load_schema: true,
+                       load_schema: false,
                        database_migrate: true
 
 Land.config.enabled = true
