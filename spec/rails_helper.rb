ad7650a28275e7ae7e3d56a15385d107f49aac71
Andrea Minhas <andrea.minhas@gmail.com>
2020-07-23 11:18:23 -0500

    simplecov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,25 +1,30 @@
 # frozen_string_literal: true
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV['RAILS_ENV'] ||= "test"
+ENV['RAILS_ENV'] ||= 'test'
 
 require 'bundler/setup'
-require "combustion"
+require 'combustion'
+require 'simplecov'
+
+SimpleCov.start 'rails' do
+  add_filter '/spec'
+end
 
 Combustion.initialize! :active_record,
-  database_reset: false,
-  load_schema: false,
-  database_migrate: true
+                       database_reset: false,
+                       load_schema: false,
+                       database_migrate: true
 
 Land.config.enabled = true
 
-require "spec_helper"
+require 'spec_helper'
 
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 
-require "pry"
-require "rspec/rails"
+require 'pry'
+require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -36,7 +41,7 @@ require "rspec/rails"
 # require only the support files necessary.
 #
 # Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -79,7 +84,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
 
   # arbitrary gems may also be filtered via:
-  config.filter_gems_from_backtrace("rspec", "rspec-rails")
+  config.filter_gems_from_backtrace('rspec', 'rspec-rails')
 
   config.backtrace_exclusion_patterns << %r{vendor/}
 end
