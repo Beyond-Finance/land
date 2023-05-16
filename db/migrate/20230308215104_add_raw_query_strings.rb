56a27b70f0c9dbe8a0bc212812e526d5b4f68176
Afra Ahmad <git@afra.io>
2023-05-16 12:03:29 -0400

    Specify schema name on vists.raw_query_string migrations

--- db/migrate/20230308215104_add_raw_query_strings.rb
+++ db/migrate/20230308215104_add_raw_query_strings.rb
@@ -1,5 +1,7 @@
 class AddRawQueryStrings < ActiveRecord::Migration[7.0]
   def change
-    add_column :visits, :raw_query_string, :text
+    with_options schema: Land.config.schema do |t|
+      add_column :visits, :raw_query_string, :text
+    end
   end
 end
