91160e2a6c4f7dd4c8a3df2ffeb04a65ab4231c4
Afra Ahmad <git@afra.io>
2023-05-16 15:11:54 -0400

    Run migration and update Ruby to 3.1.3

--- db/migrate/20230308215104_add_raw_query_strings.rb
+++ db/migrate/20230308215104_add_raw_query_strings.rb
@@ -1,7 +1,5 @@
 class AddRawQueryStrings < ActiveRecord::Migration[7.0]
   def change
-    with_options schema: Land.config.schema do |t|
-      add_column :visits, :raw_query_string, :text
-    end
+    add_column 'land.visits', :raw_query_string, :text
   end
 end
