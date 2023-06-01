f38190f8ad280c0e9cc1521ad87b198ed2f21cc2
Alex George Randolph <alexgeorgerandolph@gmail.com>
2023-06-01 17:37:32 -0600

    Fix other typo in migration

--- db/migrate/20230526200145_remove_ad_type_index_on_attributions.rb
+++ db/migrate/20230526200145_remove_ad_type_index_on_attributions.rb
@@ -1,5 +1,5 @@
 class RemoveAdTypeIndexOnAttributions < ActiveRecord::Migration[7.0]
   def change
-    remove_index 'land.attributions', column: :ad_type_id if index_exists?('land.attribtions', :ad_type_id)
+    remove_index 'land.attributions', column: :ad_type_id if index_exists?('land.attributions', :ad_type_id)
   end
 end
